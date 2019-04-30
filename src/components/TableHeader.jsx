d3311d86951968bfedca85d8973b1e0b2c88d4e2
Karim Hussain <hasham90@hotmail.com>
2019-04-30 14:58:07 +0200

    Add unique key with uuid and component rework

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
+const uuidv1 = require('uuid/v1');
+
 const TableHeader = ({ headers }) => {
   const headerColumns = () =>
-    headers.map(header => <th id={header.id}>{header.label}</th>);
-  return (
-    <thead>
-      <tr>{headerColumns()}</tr>
-    </thead>
-  );
+    headers.map(header => (
+      <div className="tableheadercolumn" key={uuidv1()} id={header.id}>
+        {header.label}
+      </div>
+    ));
+  return <div className="tableheader">{headerColumns()}</div>;
 };
 
 export default TableHeader;
