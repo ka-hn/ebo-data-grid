dfe3a882051e975acf66ccb805bfbe3e4924a2d6
Karim Hussain <hasham90@hotmail.com>
2019-05-03 14:54:58 +0200

    Enable sharing and recieving data between comps

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -12,7 +12,9 @@ class App extends Component {
       headers: [],
       data: [],
       searchfield: '',
+      sort: { order: 'asc', column: 'id' },
     };
+    this.updateSortSettings = this.updateSortSettings.bind(this);
   }
 
   componentDidMount() {
@@ -26,13 +28,11 @@ class App extends Component {
 
     const data = TestData.map(marker => {
       return {
-        id: marker.markerid,
-        title: marker.title,
-        address: `${marker.adress}, ${marker.postalnumber}, ${
-          marker.postaltown
-        } `,
-        district: marker.tags[0],
-        type: marker.type,
+        id: marker.markerid.trim(),
+        title: marker.title.trim(),
+        address: `${marker.adress.trim()}, ${marker.postalnumber.trim()}, ${marker.postaltown.trim()}`.trim(),
+        district: marker.tags[0].trim(),
+        type: marker.type.trim(),
       };
     });
 
@@ -43,12 +43,40 @@ class App extends Component {
     this.setState({ searchfield: event.target.value });
   };
 
+  onSortChange = () => {
+    const { sort } = this.state;
+    const { order } = sort;
+    const key = sort.column;
+    // TODO: For some reason this stupid function returns "Lotterivägen 15, 129 32, Hägersten" as first object when sorting on address...
+    return function compare(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        return 0;
+      }
+
+      const varA = typeof a[key] === 'string' ? a[key].toLowerCase() : a[key];
+      const varB = typeof b[key] === 'string' ? b[key].toLowerCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return order === 'desc' ? comparison * -1 : comparison;
+    };
+  };
+
+  updateSortSettings(newSettings) {
+    const { data } = this.state;
+    this.setState(newSettings);
+    data.sort(this.onSortChange());
+  }
+
   render() {
     const title = 'EBO Data Grid';
-
-    const { headers, data, searchfield } = this.state;
-
-    const filteredData = data.filter(object => {
+    const { headers, data, searchfield, sort } = this.state;
+    const sortedData = data.sort(this.onSortChange());
+    const filteredData = sortedData.filter(object => {
       return JSON.stringify(object)
         .toLowerCase()
         .includes(searchfield.toLowerCase());
@@ -60,7 +88,11 @@ class App extends Component {
       <div className="app">
         <TableTop title={title} searchChange={this.onSearchChange} />
         <div className="table">
-          <TableHeader headers={headers} />
+          <TableHeader
+            headers={headers}
+            sort={sort}
+            updateSortSettings={this.updateSortSettings}
+          />
           <Table data={filteredData} />
         </div>
       </div>
