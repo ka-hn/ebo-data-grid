4086ff4f635e0468c736b981d12b4ab5150297c1
Karim Hussain <hasham90@hotmail.com>
2019-05-10 13:32:11 +0200

    Add possibility to pass settings from TGML

--- src/components/TableTop.jsx
+++ src/components/TableTop.jsx
@@ -2,9 +2,16 @@
 import React from 'react';
 import SearchBox from './SearchBox';
 
-const TableTop = ({ title, searchChange }) => {
+const TableTop = ({ title, searchChange, settings }) => {
+  let style = {};
+  if (settings) {
+    style = {
+      background: settings.themecolor,
+    };
+  }
+
   return (
-    <div className="tabletop">
+    <div className="tabletop" style={style}>
       <h2 className="project-title">{title}</h2>
       <SearchBox searchChange={searchChange} />
     </div>
